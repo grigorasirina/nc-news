c5ad8b3a8363b834c134530516467cf149f3c76d
irina <grigorasirina17@gmail.com>
2025-05-23 15:06:24 +0000

    View a separate page for each topic with a list of related articles

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,21 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePage from './components/pages/HomePage.jsx';
 import Header from './components/layout/Header.jsx';
 import SingleArticlePage from './components/pages/SingleArticlePage.jsx';
+import ArticlesByTopicPage from './components/pages/ArticlesByTopicPage.jsx';
+import TopicNav from './components/features/TopicNav.jsx';
 import './App.css';
-
-const ArticlesByTopicPage = () => <div>Articles By Topic Page (Coming Soon!)</div>;
 
 const App = () => {
 
   return (
     <BrowserRouter>
       <Header />
+      <TopicNav />
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/articles/:article_id" element={<SingleArticlePage />} />
-          <Route path="/topics/:topic_slug" element={<ArticlesByTopicPage />} />
+          <Route path="/articles/topic/:topic_slug" element={<ArticlesByTopicPage />} />
         </Routes>
       </div>
     </BrowserRouter>
